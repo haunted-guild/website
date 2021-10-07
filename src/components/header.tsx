23d45d14aa098d7672d3bb52959c0b8dc0d77268
Michael Herwig <github.dev@michael-herwig.de>
2021-10-07 11:35:57 +0200

    add facebook link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { Nav, Navbar, Container } from "react-bootstrap"
 import PropTypes from "prop-types"
 
-import { FaTwitter, FaTwitch, FaEnvelope } from "react-icons/fa"
+import { FaTwitter, FaTwitch, FaFacebook, FaEnvelope } from "react-icons/fa"
 import { Link as GatsbyLink } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
@@ -54,6 +54,11 @@ const Header = ({ siteTitle }) => (
           <Nav.Item>
             <Nav.Link href="https://twitter.com/hauntedguild" target="_blank">
               <FaTwitter />
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="https://www.facebook.com/Haunted-104616588663255/" target="_blank">
+              <FaFacebook />
             </Nav.Link>
           </Nav.Item>
           {/* <Nav.Item>
